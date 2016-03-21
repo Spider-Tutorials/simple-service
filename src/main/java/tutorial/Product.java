a50fc49f8cf9d38bd377f192a6259b36d0db0825
tutorial-1 <mouse0407@163.com>
2016-03-22 00:56:45 +0800

    added DTO

---
+++ src/main/java/tutorial/Product.java
@@ -0,0 +1,31 @@
+package tutorial;
+
+public class Product {
+    private String name;
+    private String description;
+    private String producer;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+}
